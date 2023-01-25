a9da137b83a60226a6299709193dde04d375ee3c
daren88888 <89655783+daren88888@users.noreply.github.com>
2023-01-25 17:26:21 -0500

    Create myApp.js

---
+++ myApp.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+
+htmlName = __dirname + "/views/index.html"
+app.get("/", function(req, res) {
+  res.sendFile(htmlName);
+const mySecret = process.env['MESSAGE_STYLE']
+})
+
+styleName = __dirname + "/public"
+app.use("/public", express.static(styleName))
+
+app.get("/json", function(req, res) {
+  var response = {"message": "Hello json"};
+  if(process.env.MESSAGE_STYLE === "uppercase") {
+    response.message = response.message.toUpperCase();
+  }
+  return res.json(response);
+  
+})
