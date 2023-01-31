d0b5eafa5a8cb5f8958e25a4f46b7b73978fd2a2
daren88888 <89655783+daren88888@users.noreply.github.com>
2023-01-31 22:42:18 +0000

    linked login page and register page

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the login page
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+})
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
