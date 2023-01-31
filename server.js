5e535f507c1372093e5af1acc5cd00d0f724bc99
daren88888 <89655783+daren88888@users.noreply.github.com>
2023-01-31 04:10:02 +0000

    adopted tim's html file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the login page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'BusinessLogin.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(3000, () => {
