99062fbe0946b895483082b3ec108ed87e41a356
daren88888 <89655783+daren88888@users.noreply.github.com>
2023-02-14 02:49:23 +0000

    log in redirect functionality and AWS database connections

--- server.js
+++ server.js
@@ -2,24 +2,40 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(session)
-// http://localhost:3000/
-app.get('/views', function(request, response) {
-	// Render login template
-	response.sendFile(path.join(__dirname, '/index.html'));
+const con = mysql.createConnection({
+    host: "parkpal-instance-1.cp4hkrqf72vo.us-west-2.rds.amazonaws.com",
+    user: "admin",
+    password: "parkpal88888"
 });
 
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // change to true in production if using HTTPS
+}));
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname, 'views/index.html'));
+});
 // http://localhost:3000/
 app.post('/auth', function(request, response) {
 	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
+	const username = request.body.username;
+	const password = request.body.password;
+  const query = `SELECT * FROM business.users WHERE username = '${username}' AND password = '${password}'`;
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
 		// Execute SQL query that'll select the business table from the database based on the specified username and password
-		con.query('SELECT * FROM business.users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		con.query(query, function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
@@ -28,7 +44,7 @@ app.post('/auth', function(request, response) {
 				request.session.loggedIn = true;
 				request.session.username = username;
 				// Redirect to home page
-				response.redirect('/');
+				response.redirect('/dashboard');
 			} else {
 				response.send('Incorrect Username and/or Password!');
 			}			
@@ -40,17 +56,15 @@ app.post('/auth', function(request, response) {
 	}
 });
 
-// http://localhost:3000/home
-app.get('/', function(request, response) {
-	// If the user is loggedin
-	if (request.session.loggedIn) {
-		// Output username
-		response.sendFile(path.join(__dirname, 'views/dashboard.html'));;
+// http://localhost:3000/dashboard
+
+app.get('/dashboard', function(request, response) {
+  if (request.session.loggedIn) {
+		response.sendFile(path.join(__dirname, 'views/dashboard.html'));
 	} else {
 		// Not logged in
-		response.send('Please login to view this page!');
+		response.redirect('/');
 	}
-	response.end();
 });
 
 app.listen(3000, () => {
